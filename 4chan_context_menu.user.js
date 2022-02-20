b22452b32db720d24556b193cd67b21e686c371a
Gitle Mikkelsen <gitlem@gmail.com>
2022-02-20 09:11:08 +0100

    small update, also adblock

--- 4chan_context_menu.user.js
+++ 4chan_context_menu.user.js
@@ -2,22 +2,26 @@
 // @name         Remove 4chan context menu items
 // @namespace    https://github.com/Grix
 // @updateURL    https://github.com/Grix/4chan-remove-contextmenu-items/raw/master/4chan_context_menu.user.js
-// @version      0.2
+// @version      0.3
 // @description  Remove 4chan context menu items
 // @author       Grixm
 // @match        https://boards.4channel.org/*/catalog
 // @match        https://boards.4chan.org/*/catalog
+// @match        https://boards.4channel.org/*
+// @match        https://boards.4chan.org/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    window.onload = setTimeout(removeMenuItems, 500);
+    window.onload = setTimeout(removeMenuItems, 3000);
 }
 )();
 
 function removeMenuItems(){
     var contextMenuNode = document.getElementById("ctxmenu-thread");
     contextMenuNode.innerHTML='';
+	var ad = document.getElementsByClassName("adg-rects desktop");
+	ad.innerHTML='';//parentNode.removeChild(ad);
 }
